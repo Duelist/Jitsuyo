1dab435eef86d2113dfbe9a440afb2bdfad31042
Ian Benedict <ianbenedict@gmail.com>
2014-05-09 08:06:52 -0400

    Updated example module to include static path.

--- modules/example/index.js
+++ modules/example/index.js
@@ -1,8 +1,10 @@
 var express = require('express'),
+    path = require('path'),
     app = module.exports = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use('/static', express.static(path.join(__dirname, '/public')));
 
 app.get('/example', function(req, res) {
   res.render('example');
